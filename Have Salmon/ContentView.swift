dbe3b9c048b3025948525733e0aae9430b776471
skyha27 <skyler_hall@brown.edu>
2024-06-26 11:08:45 -0400

    Initial Commit

---
+++ Have Salmon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Have Salmon
+//
+//  Created by Skyler Hall on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Have Salmon/Have_SalmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Have_SalmonApp.swift
+//  Have Salmon
+//
+//  Created by Skyler Hall on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Have_SalmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
